051874864701ee040e83abdfd6c604a2ad5d6d98
MedCharaf <bouderraoui45@gmail.Com>
2023-01-30 01:56:10 +0100

    Update client.c
    3refhan kisendi bitats

--- client.c
+++ client.c
@@ -1,1 +1,50 @@
-#client
+#include "minitalk.h"
+
+ static void send(int pid, char *message)
+{
+    int bit;
+    int i = 0;
+
+    
+
+    while (message[i] != '\0')        
+    {
+        
+        bit = 0;
+        while (bit <= 8)
+        {
+            
+
+            if ((message[i] & (1 << bit)) != 0)
+
+                kill(pid, SIGUSR1);
+            else
+                kill(pid, SIGUSR2);
+                
+            usleep(100);
+
+            bit++;
+        }
+        i++;
+     
+    }
+}
+int main(int argc, char **argv)
+{
+    int pid;
+    int i;
+
+    i = 0;
+    if (argc == 3)
+    {
+        pid = ft_atoi(argv[1]);
+
+        send(pid, argv[2]);
+        return 0;
+    }
+    else
+    {
+        printf("bad format");
+        exit(0);
+    }
+}
