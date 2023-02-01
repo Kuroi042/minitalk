95fb99977d6b1d9a60423dc472e35194ee62a597
MedCharaf <bouderraoui45@gmail.Com>
2023-02-01 06:57:14 +0100

    finnaly its printing finnaly its printing finnaly its printing
    finnaly its printing

--- server.c
+++ server.c
@@ -1,54 +1,58 @@
-#include "minitalk.h"      
+#include "minitalk.h"
 
+unsigned char bitsdecoder(int *bits)
+{
+   unsigned char c;
+   int res = 0;
+   int j = 7;
+   while (j >= 0)
+   {
+      res += (bits[j] * ft_power(2, j));
+      j--;
+   }
+   c = (unsigned char)res;
+   return c;
+}
 void sig_handler(int sig)
 {
-    static char bits[7];
+    
     static int i = 0;
-    if (i <= 7)
+    static int arr[8];
+    if (sig == SIGUSR1)
     {
-        if (sig == SIGUSR1)
-        {
-            bits[i] = '1';
-        }
-        else if (sig == SIGUSR2)
-        {
-            bits[i] = '0';
-        }
-
-        i++;
+        arr[i++] = 1;
     }
-
-    if (i == 7)
+    else if (sig == SIGUSR2)
     {
-        bits[i] = '\0';
-        printf("Server received message: %s\n", bits);
+        arr[i++] = 0;
+    }
+    if (i == 8)
+    {
+        //write(1,"\n",1);
         i = 0;
+        unsigned char decoded = bitsdecoder(arr);
+        write(1,&decoded,1);    
+        ft_bzero(arr, 8);
     }
 }
 
-int main(int argc , char *argv[]) 
+int main(int argc, char *argv[])
 {
-  struct sigaction sa;
-  sa.sa_handler = &sig_handler;
-  sigaction(SIGUSR1, &sa, NULL);
-  sigaction(SIGUSR2, &sa, NULL);
-
-
-    if(argc != 1)
-    {
-        write(1,"wawawawawa",6);
-    }
-  
 (void)**argv;
+   struct sigaction sa;
+   sa.sa_handler = &sig_handler;
+   sigaction(SIGUSR1, &sa, NULL);
+   sigaction(SIGUSR2, &sa, NULL);
 
-int pid = getpid();
-if(argc == 1)
-{
-    ft_putnbr(pid);
-   write(1,"\nwaiting...\n",15);
-  while(1)
-    {
-     pause();
-    }
-}
+   int pid = getpid();
+   if (argc == 1)
+   {
+      printf("%d\n", pid);
+      printf("waiting...\n");
+      while (1)
+      {
+         pause();
+      }
+   }
+   return 0;
 }
