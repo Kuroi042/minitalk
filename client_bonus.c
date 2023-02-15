34544667be1762cbd3cda24b443d9e1e8485ad81
Mohamed charaf Bouderraoui <mbouderr@c2r1p10.1337.ma>
2023-02-16 00:10:36 +0100

    client print msg back and respect the norms

--- client_bonus.c
+++ client_bonus.c
@@ -1,70 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbouderr <mbouderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 18:19:54 by mbouderr          #+#    #+#             */
+/*   Updated: 2023/02/15 21:58:32 by mbouderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
-size_t incr = 0;
+size_t		g_incr = 0;
 
-void signalback(int sig)
+void	signalback(int sig)
 {
 	if (sig == SIGUSR1)
 	{
-		incr++;
-   		//write(1, "BYTE RECIEVED\n", 16);
+		g_incr++;
 	}
 }
 
-static void sender(int pid, char *message, size_t len)
+static void	sender(int pid, char *message, size_t len)
 {
-    int bit;
-    int i = 0;
-    while (message[i] != '\0')
-    {
-        bit = 0;
-        while (bit < 8)
-        {
-            if ((message[i] & (1 << bit)) != 0)
-                kill(pid, SIGUSR1);
-            else
-                kill(pid, SIGUSR2);
-            usleep(150);
-            bit++;
-        }
-        i++;
-    }
-	if (incr == len)
+	int	bit;
+	int	i;
+
+	i = 0;
+	while (message[i] != '\0')
 	{
-		write(1,"<<yes>>",7);
-		exit(0);
+		bit = 0;
+		while (bit < 8)
+		{
+			if ((message[i] & (1 << bit)) != 0)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(150);
+			bit++;
+		}
+		i++;
 	}
-	if (incr != len)
-	{
+	if (g_incr == len)
+		write(1, "<<yes>>", 7);
+	exit(0);
+	if (g_incr != len)
 		write(1, "<<no>>", 6);
-		exit(1);
-	}
+	exit(1);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    int pid;
-	struct sigaction sa;
+	int					pid;
+	size_t				len;
+	struct sigaction	sa;
 
 	sa.sa_handler = signalback;
 	sa.sa_flags = 0;
-
-    if (argc == 3)
-    {
-        pid = ft_atoi(argv[1]);
-        if (pid < 2)
-        {
-            write(1, "pid is not valid :( ", 22);
-            exit(0);
-        }
-        size_t len = ft_strlen(argv[2]);
-        sigaction(SIGUSR1, &sa, NULL);
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		if (pid < 2)
+		{
+			write(1, "pid is not valid :( ", 22);
+			exit(0);
+		}
+		len = ft_strlen(argv[2]);
+		sigaction(SIGUSR1, &sa, NULL);
 		sender(pid, argv[2], len);
-    }
-    else
-    {
-        write(1, "invalid format\n", 17);
-        exit(0);
-    }
-	return 0;
+	}
+	else
+	{
+		write(1, "invalid format\n", 16);
+		exit(0);
+	}
+	return (0);
 }
