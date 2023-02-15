26891f6003e4de17fa746dbe04ba9845310c877d
Mohamed charaf Bouderraoui <mbouderr@c2r1p10.1337.ma>
2023-02-16 00:04:30 +0100

    client respect normes and sends bits bl9a3ida

--- client.c
+++ client.c
@@ -1,46 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbouderr <mbouderr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/15 18:39:16 by mbouderr          #+#    #+#             */
+/*   Updated: 2023/02/15 18:39:47 by mbouderr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minitalk.h"
 
- static void send(int pid, char *message)
+static void	send(int pid, char *message)
 {
-    int bit;
-    int i = 0;
-    while (message[i] != '\0')        
-    {
-        
-        bit = 0;
-        while (bit < 8)
-        {
-            if ((message[i] & (1 << bit)) != 0)
-        
-                kill(pid, SIGUSR1);
-            else
-                kill(pid, SIGUSR2);             
-            usleep(100);
-            bit++;
-        }
-        i++;
-    }
-}
-int main(int argc, char **argv)
-{
-    int pid;
-    int i;
+	int	bit;
+	int	i;
 
-    i = 0;
-    if (argc == 3)
-    {
-        pid = ft_atoi(argv[1]);
-     if(pid < 2)
-     {
-         write(1,"pid is not valid :( ",20);
-        exit(0);
-     }
-        send(pid, argv[2]);
-        return 0;
-    }
-    else
-    {
-        printf("bad format");
-        exit(0);
-    }
+	i = 0;
+	while (message[i] != '\0')
+	{
+		bit = 0;
+		while (bit < 8)
+		{
+			if ((message[i] & (1 << bit)) != 0)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(150);
+			bit++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	i = 0;
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		if (pid < 2)
+		{
+			write(1, "pid is not valid :( ", 20);
+			exit(0);
+		}
+		send(pid, argv[2]);
+		return (0);
+	}
+	else
+	{
+		printf("bad format");
+		exit(0);
+	}
+	return (0);
 }
